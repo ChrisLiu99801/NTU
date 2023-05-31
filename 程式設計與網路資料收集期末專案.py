6f8c7aeb3af6ae891fa195b02dae70f651ca718d
ChrisLiu99801 <134841572+ChrisLiu99801@users.noreply.github.com>
2023-05-31 10:32:16 +0800

    Update 程式設計與網路資料收集期末專案.py

--- 程式設計與網路資料收集期末專案.py
+++ 程式設計與網路資料收集期末專案.py
@@ -8,7 +8,19 @@ from datetime import datetime
 import math
 figsize = (14, 8) 
 
-st.markdown("trend.png", unsafe_allow_html=True)
+st.markdown(
+    """
+    <style>
+    .reportview-container {
+        background: url("https://images.app.goo.gl/LFCobouKtT7oZ7Qv7")
+    }
+   .sidebar .sidebar-content {
+        background: url("https://images.app.goo.gl/LFCobouKtT7oZ7Qv7")
+    }
+    </style>
+    """,
+    unsafe_allow_html=True
+)
 
 st.set_page_config(
     page_title="Portfolio Analysis",
