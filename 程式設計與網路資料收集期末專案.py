18c37036e56be7ecbc29bf97bb219a6090a90791
ChrisLiu99801 <134841572+ChrisLiu99801@users.noreply.github.com>
2023-05-31 13:43:59 +0800

    Update 程式設計與網路資料收集期末專案.py

--- 程式設計與網路資料收集期末專案.py
+++ 程式設計與網路資料收集期末專案.py
@@ -8,22 +8,28 @@ from datetime import datetime
 import math
 figsize = (14, 8) 
 
-def sidebar_bg(side_bg):
-
-   side_bg_ext = 'png'
-
-   st.markdown(
-      f"""
-      <style>
-      [data-testid="stSidebar"] > div:first-child {{
-          background: url(https://github.com/ChrisLiu99801/NTU/blob/c48b09a089aabef17dbe56dc1f5476c48c2896ba/trend.png).read()).decode()});
-      }}
-      </style>
-      """,
-      unsafe_allow_html=True,
-      )
-
-
+def set_bg_hack_url():
+    '''
+    A function to unpack an image from url and set as bg.
+    Returns
+    -------
+    The background.
+    '''
+        
+    st.markdown(
+         f"""
+         <style>
+         .stApp {{
+             background: url("https://github.com/ChrisLiu99801/NTU/blob/c48b09a089aabef17dbe56dc1f5476c48c2896ba/trend.png");
+             background-size: cover
+         }}
+         </style>
+         """,
+         unsafe_allow_html=True
+     )
+   
+ set_bg_hack_url()
+   
 st.set_page_config(
     page_title="Portfolio Analysis",
     page_icon="random",
