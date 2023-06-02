265bb70fd320df6fe2ab1a70f87ba99e320f6b66
ChrisLiu99801 <134841572+ChrisLiu99801@users.noreply.github.com>
2023-06-03 00:16:32 +0800

    Update 程式設計與網路資料收集期末專案.py

--- 程式設計與網路資料收集期末專案.py
+++ 程式設計與網路資料收集期末專案.py
@@ -14,8 +14,8 @@ st.set_page_config(
     page_icon="random",
     layout="centered"
 )
-
-st.image(S__17105008.jpg, caption='Sunrise by the mountains')
+image = pd.read_csv("S__17105008.jpg")
+st.image(image, caption='Sunrise by the mountains')
 
 st.markdown("<h1 style='text-align: center; color: black;'>Analysmart 投資組合分析</h1>", unsafe_allow_html=True)
 st.markdown("<h1 style='text-align: center; color: black;'> </h1>", unsafe_allow_html=True)
